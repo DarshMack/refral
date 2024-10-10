c47691174c460d16cffc90a2c68729397f339558
Darsh_Mack <darshanmakwana8796@gmail.com>
2024-10-10 11:47:35 +0530

    update

--- module/models/userqueries.js
+++ module/models/userqueries.js
@@ -1,76 +1,82 @@
-import connection from '../../config/database.js';
-import jwt from 'jsonwebtoken';
-
+import connection from "../../config/database.js";
+import jwt from "jsonwebtoken";
 
 export const addUser = (req) => {
-    return new Promise((resolve, reject) => {
-
-        var params = {
-            name : req.name,
-            mobile : req.mobile,
-            referralCode : req.referrerCode,
-            gender : req.gender, 
-            technology : JSON.stringify(req.technology), 
-            profilePic : JSON.stringify(req.profilePic), 
-            dob: req.dob, 
-            points : req.referrerPoints
-        }
-        connection.query(`INSERT INTO tbl_user SET ?`, params, (err, result) => {  
-            if (err) {
-                reject(err)
-            }else{
-                if (req.referralPointsId == 0) {
-                    const jwtToken = jwt.sign({ id: result.insertId }, process.env.JWT_SECRET);
-                    result.jwtToken = jwtToken
-                    resolve(result);
-                }else{
-                    connection.query('UPDATE tbl_user SET points = points + ' + req.referralPoints + ' WHERE id ="' + req.referralPointsId + '"', function(err1, result1) {
-                        const jwtToken = jwt.sign({ id: result.insertId }, process.env.JWT_SECRET);
-                        result.jwtToken = jwtToken
-                        resolve(result);
-                    })
-                }
+  return new Promise((resolve, reject) => {
+    var params = {
+      name: req.name,
+      mobile: req.mobile,
+      referralCode: req.referrerCode,
+      gender: req.gender,
+      technology: JSON.stringify(req.technology),
+      profilePic: JSON.stringify(req.profilePic),
+      dob: req.dob,
+      points: req.referrerPoints,
+    };
+    connection.query(`INSERT INTO tbl_user SET ?`, params, (err, result) => {
+      if (err) {
+        reject(err);
+      } else {
+        const jwtToken = jwt.sign(
+          { id: result.insertId },
+          process.env.JWT_SECRET
+        );
+        result.jwtToken = jwtToken;
+        if (req.referralPointsId == 0) {
+          resolve(result);
+        } else {
+          connection.query(
+            "UPDATE tbl_user SET points = points + " +
+              req.referralPoints +
+              ' WHERE id ="' +
+              req.referralPointsId +
+              '"',
+            function (err1, result1) {
+              resolve(result);
             }
-        });
+          );
+        }
+      }
     });
+  });
 };
 
 export const getReferrals = (page, limit) => {
-    return new Promise((resolve, reject) => {
-        const offset = (page - 1) * limit;
-        const query = `SELECT * FROM tbl_user LIMIT ?, ?`;
-        connection.query(query, [offset, parseInt(limit)], (err, results) => {
-            if (err) reject(err);
-            resolve(results);
-        });
+  return new Promise((resolve, reject) => {
+    const offset = (page - 1) * limit;
+    const query = `SELECT * FROM tbl_user LIMIT ?, ?`;
+    connection.query(query, [offset, parseInt(limit)], (err, results) => {
+      if (err) reject(err);
+      resolve(results);
     });
+  });
 };
 
 export const removeUser = (id) => {
-    return new Promise((resolve, reject) => {
-        const query = `DELETE FROM tbl_user WHERE id = ?`;
-        connection.query(query, [id], (err, result) => {
-            if (err) reject(err);
-            resolve(result);
-        });
+  return new Promise((resolve, reject) => {
+    const query = `DELETE FROM tbl_user WHERE id = ?`;
+    connection.query(query, [id], (err, result) => {
+      if (err) reject(err);
+      resolve(result);
     });
+  });
 };
 
 export const updateUser = (req) => {
-    return new Promise((resolve, reject) => {
-      var params = {
-        name : req.name,
-        mobile : req.mobile,
-        gender : req.gender, 
-        technology : JSON.stringify(req.technology), 
-        profilePic : JSON.stringify(req.profilePic), 
-        dob: req.dob 
-    }
+  return new Promise((resolve, reject) => {
+    var params = {
+      name: req.name,
+      mobile: req.mobile,
+      gender: req.gender,
+      technology: JSON.stringify(req.technology),
+      profilePic: JSON.stringify(req.profilePic),
+      dob: req.dob,
+    };
 
-        const query = `UPDATE tbl_user SET ? WHERE id = '`+req.id+`'`;
-        connection.query(query, params, (err, result) => {
-            if (err) reject(err);
-            resolve(result);
-        });
+    const query = `UPDATE tbl_user SET ? WHERE id = '` + req.id + `'`;
+    connection.query(query, params, (err, result) => {
+      if (err) reject(err);
+      resolve(result);
     });
+  });
 };
--- server.js
+++ server.js
@@ -1,28 +1,77 @@
-import express from 'express';
-import cluster from 'cluster';
-import os from 'os';
-import dotenv from 'dotenv';
-import userRoutes from './module/routes/userroutes.js';
+import express from "express";
+import cluster from "cluster";
+import os from "os";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import multer from "multer";
+import path from "path";
+import { fileURLToPath } from "url";
+import userRoutes from "./module/routes/userroutes.js";
+import {
+  saveUserData,
+  fetchReferralUsers,
+  deleteUser,
+  updateProfile,
+} from "./module/controllers/usercontroller.js";
 
 dotenv.config();
 
 const app = express();
 const numCPUs = os.cpus().length;
 
+// Configuring __dirname for ES6 modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 if (cluster.isMaster) {
-    
-    for (let i = 0; i < numCPUs; i++) {
-        cluster.fork();
-    }
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
 
-    cluster.on('exit', (worker, code, signal) => {
-        console.log(`Worker ${worker.process.pid} died`);
-    });
+  cluster.on("exit", (worker, code, signal) => {
+    console.log(`Worker ${worker.process.pid} died`);
+  });
 } else {
-    app.use(express.json());
-    app.use('/api', userRoutes);
+  app.use(express.json());
+  app.use("/api", userRoutes);
 
-    app.listen(process.env.PORT || 3000, () => {
-        console.log(`Worker ${process.pid} is running on port ${process.env.PORT || 3000}`);
-    });
+  // Serve static files
+  app.use(express.static(path.join(__dirname, "public")));
+
+  // Multer setup for handling file uploads
+  const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, "./uploads");
+    },
+    filename: (req, file, cb) => {
+      cb(null, Date.now() + path.extname(file.originalname)); // Add timestamp to avoid file conflicts
+    },
+  });
+
+  const upload = multer({ storage });
+
+  // Render the form HTML
+  app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+  });
+
+  // Route to handle form submission
+  app.post("/submit", upload.array("profilePic", 5), async (req, res) => {
+    const { name, mobile, referralCode, gender, dob } = req.body;
+    const profilePics = req.files.map((file) => file.filename);
+    req.body.profilePic = profilePics;
+    try {
+      const result = await saveUserData(req, res);
+      res.status(200).json({ message: "User added successfully", result });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ error: error });
+    }
+  });
+
+  app.listen(process.env.PORT || 3000, () => {
+    console.log(
+      `Worker ${process.pid} is running on port ${process.env.PORT || 3000}`
+    );
+  });
 }
